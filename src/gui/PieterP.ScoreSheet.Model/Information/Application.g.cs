6ccbfaeca2caf7a5598e891e54ad87c92753ee6c
Pieter Philippaerts <pp@pieterp.be>
2020-09-14 17:12:38 +0200

    v2020.2021.4

--- src/gui/PieterP.ScoreSheet.Model/Information/Application.g.cs
+++ src/gui/PieterP.ScoreSheet.Model/Information/Application.g.cs
@@ -6,7 +6,7 @@ namespace PieterP.ScoreSheet.Model.Information {
     public static partial class Application {
         public static DateTime CompilationTimestamp { 
 			get { 
-				return new DateTime(637356925957882537L, DateTimeKind.Utc); 
+				return new DateTime(637356926840304644L, DateTimeKind.Utc); 
 			}
 		}
     }
