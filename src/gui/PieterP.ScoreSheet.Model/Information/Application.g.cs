434681c09e21ad28c384b138457d62a9d75feacc
Pieter Philippaerts <pp@pieterp.be>
2021-08-23 21:14:50 +0200

    Verbetering van het VTTL-wedstrijdblad voor mathcen met meer dan 5 sets...

--- src/gui/PieterP.ScoreSheet.Model/Information/Application.g.cs
+++ src/gui/PieterP.ScoreSheet.Model/Information/Application.g.cs
@@ -6,7 +6,7 @@ namespace PieterP.ScoreSheet.Model.Information {
     public static partial class Application {
         public static DateTime CompilationTimestamp { 
 			get { 
-				return new DateTime(637653279473423773L, DateTimeKind.Utc); 
+				return new DateTime(637653426153085230L, DateTimeKind.Utc); 
 			}
 		}
     }
