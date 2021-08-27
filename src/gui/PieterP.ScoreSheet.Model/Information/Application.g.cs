1eb662ecf770aef7c5172b19115ba91894597210
Pieter Philippaerts <pp@pieterp.be>
2021-08-27 14:10:42 +0200

    Fix template vrije tijd

--- src/gui/PieterP.ScoreSheet.Model/Information/Application.g.cs
+++ src/gui/PieterP.ScoreSheet.Model/Information/Application.g.cs
@@ -6,7 +6,7 @@ namespace PieterP.ScoreSheet.Model.Information {
     public static partial class Application {
         public static DateTime CompilationTimestamp { 
 			get { 
-				return new DateTime(637656620816851922L, DateTimeKind.Utc); 
+				return new DateTime(637656629955997194L, DateTimeKind.Utc); 
 			}
 		}
     }
