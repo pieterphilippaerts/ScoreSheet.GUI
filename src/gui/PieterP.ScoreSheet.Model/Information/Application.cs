28bf62077a91e625c1c062b2f9905b1f8d14fbc5
Pieter Philippaerts <pp@pieterp.be>
2024-08-22 14:36:49 +0200

    Update competition year 2024.2025

--- src/gui/PieterP.ScoreSheet.Model/Information/Application.cs
+++ src/gui/PieterP.ScoreSheet.Model/Information/Application.cs
@@ -17,13 +17,13 @@ namespace PieterP.ScoreSheet.Model.Information {
         }
         public static DateTime ExpiryTime {
             get {
-                return new DateTime(2024, 9, 1); // expires at the start of season 2021-2022
+                return new DateTime(2025, 9, 1); // expires at the start of season 2021-2022
                 // if you're updating this, also update the DefaultSeasonId below
             }
         }
         public static int DefaultSeasonId {
             get {
-                return 24; // the ID of season 2023-2024
+                return 25; // the ID of season 2023-2024
             }
         }
         public static int DefaultMenCategoryId {
